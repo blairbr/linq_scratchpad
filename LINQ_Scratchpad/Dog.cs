74be61a7d50dceb923f127b1ea62cd98fea4bad7
Blair Brown <blair.j.brown@gmail.com>
2021-01-05 23:55:51 -0500

    select many start

---
+++ LINQ_Scratchpad/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_Scratchpad
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public List<string> Skills {get; set;}
+
+        public Dog()
+        {
+
+        }
+        public Dog(string name) //, List<string> skills)
+        {
+            Name = name;
+           // Skills = skills;
+        }
+    }
+}
---
+++ LINQ_Scratchpad/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace LINQ_Scratchpad
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Dog fido = new Dog();
+
+            Dog rex = new Dog
+            {
+                Name = "Rex",
+                Skills = { "playing ball", "sniffing" }
+            };
+
+        }
+
+        public void GetSkills() {
+        }
+        }
+
+  
+    }
+
+            //public async Task<List<ExtensionValidationSetNode>> GetAllExtensionValidationSetNodes(IExtension source, CancellationToken cancellationToken)
+            //{
+            //    var validationSets = _dataProvider.GetAllValidationSets();
+            //    var result = new List<ExtensionValidationSetNode>();
+            //    var alternateValues = _alternateValueProvider.GetAlternateValuesForExtensionAsync(RequestContext.Internal, source.Id, cancellationToken).Result;
+            //    foreach (var valset in validationSets)
+            //    {
+            //        var usedRecordCount = 0;
+            //        var valSetEntries = await GetEntriesForValidationSetAsync(valset.Id, cancellationToken).ConfigureAwait(false);
+            //        var numberOfValsetEntries = valSetEntries.Count;
+            //        foreach (var entry in valSetEntries)
+            //        {
+            //            foreach (var altValue in alternateValues)
+            //            {
+            //                if (altValue.UsageTypeId == UsageType.ValidationSetEntry && altValue.RecordId == entry.Id)
+            //                {
+            //                    usedRecordCount++;
+            //                }
+            //            }
+            //        }
+            //        result.Add(new ExtensionValidationSetNode
+            //        {
+            //            ValidationSetName = valset.Name,
+            //            ValidationSetId = valset.Id,
+            //            TotalRecordCount = numberOfValsetEntries, // the number of Validation Set Entries that belong to that validation set
+            //            UsedRecordCount = usedRecordCount  // the number of ValSetEntries that are tied to an alterate value
+            //        });
+            //    }
+            //    return result;
+            //}
+
+            //public async Task<List<ExtensionValidationSetNode>> GetAllExtensionValidationSetNodesREFACTORED(IExtension source, CancellationToken cancellationToken)
+            //{
+            //    var validationSets = _dataProvider.GetAllValidationSets();
+            //    var result = new List<ExtensionValidationSetNode>();
+            //    var alternateValues = _alternateValueProvider.GetAlternateValuesForExtensionAsync(RequestContext.Internal, source.Id, cancellationToken).Result;
+
+            //    var extensionValidationSetNode = new ExtensionValidationSetNode();
+                
+            //    foreach (var valset in validationSets)
+            //    {
+            //        var usedRecordCount = 0;
+            //        var valSetEntries = await GetEntriesForValidationSetAsync(valset.Id, cancellationToken).ConfigureAwait(false);
+            //        var numberOfValsetEntries = valSetEntries.Count;
+            //        foreach (var entry in valSetEntries)
+            //        {
+            //            foreach (var altValue in alternateValues)
+            //            {
+            //                if (altValue.UsageTypeId == UsageType.ValidationSetEntry && altValue.RecordId == entry.Id)
+            //                {
+            //                    usedRecordCount++;
+            //                }
+            //            }
+            //        }
+            //        result.Add(new ExtensionValidationSetNode
+            //        {
+            //            ValidationSetName = valset.Name,
+            //            ValidationSetId = valset.Id,
+            //            TotalRecordCount = numberOfValsetEntries, // the number of Validation Set Entries that belong to that validation set
+            //            UsedRecordCount = usedRecordCount  // the number of ValSetEntries that are tied to an alterate value
+            //        });
+            //    }
+            //    return result;
+            //}
+        }
+    }
+
