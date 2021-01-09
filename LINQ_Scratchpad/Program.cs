b48f843a07eaa92479b1a766d21c85b1a9511c43
Blair Brown <blair.j.brown@gmail.com>
2021-01-08 23:23:59 -0500

    added filter dog by name using .Where method

--- LINQ_Scratchpad/Program.cs
+++ LINQ_Scratchpad/Program.cs
@@ -11,87 +11,24 @@ namespace LINQ_Scratchpad
         {
             List<Dog> listOfDogs = new List<Dog>()
             {
-            new Dog("Rex", new List<string> {"ball", "sniffing"}),
-            new Dog("Peanut", new List<string> { "Frisbee", "fetch" })
+                new Dog("Rex", new List<string> {"ball", "sniffing"}),
+                new Dog("Peanut", new List<string> { "Frisbee", "fetch" })
             };
-            GetDogSkills(listOfDogs);
+            // calling without the optional parameter will default to filtering on the name Peanut
+            FilterDogsByNameUsingWHERE(listOfDogs);
 
+            // calling this method and passing in Rex will filter on Rex
+            FilterDogsByNameUsingWHERE(listOfDogs, "Rex");
         }
 
-        public static void GetDogSkills(List<Dog> listOfDogs)
+        public static void FilterDogsByNameUsingWHERE(List<Dog> listOfDogs, string nameToFilterOn = "Peanut")
         {
-            var getallpeanuts = listOfDogs.Where(c => c.Name == "Peanut").Select(s => s.Name);
-
-            //var selectedDoggos = listOfDogs.SelectMany(i => i.Name);
-            Console.WriteLine(getallpeanuts);
+            var listOfDogsNamedPeanut = listOfDogs.Where(c => c.Name == nameToFilterOn).ToList();
+            foreach (var dog in listOfDogsNamedPeanut)
+            {
+                Console.WriteLine(dog.Name);
+            }
         }
     }
-
-
 }
-
-            //public async Task<List<ExtensionValidationSetNode>> GetAllExtensionValidationSetNodes(IExtension source, CancellationToken cancellationToken)
-            //{
-            //    var validationSets = _dataProvider.GetAllValidationSets();
-            //    var result = new List<ExtensionValidationSetNode>();
-            //    var alternateValues = _alternateValueProvider.GetAlternateValuesForExtensionAsync(RequestContext.Internal, source.Id, cancellationToken).Result;
-            //    foreach (var valset in validationSets)
-            //    {
-            //        var usedRecordCount = 0;
-            //        var valSetEntries = await GetEntriesForValidationSetAsync(valset.Id, cancellationToken).ConfigureAwait(false);
-            //        var numberOfValsetEntries = valSetEntries.Count;
-            //        foreach (var entry in valSetEntries)
-            //        {
-            //            foreach (var altValue in alternateValues)
-            //            {
-            //                if (altValue.UsageTypeId == UsageType.ValidationSetEntry && altValue.RecordId == entry.Id)
-            //                {
-            //                    usedRecordCount++;
-            //                }
-            //            }
-            //        }
-            //        result.Add(new ExtensionValidationSetNode
-            //        {
-            //            ValidationSetName = valset.Name,
-            //            ValidationSetId = valset.Id,
-            //            TotalRecordCount = numberOfValsetEntries, // the number of Validation Set Entries that belong to that validation set
-            //            UsedRecordCount = usedRecordCount  // the number of ValSetEntries that are tied to an alterate value
-            //        });
-            //    }
-            //    return result;
-            //}
-
-            //public async Task<List<ExtensionValidationSetNode>> GetAllExtensionValidationSetNodesREFACTORED(IExtension source, CancellationToken cancellationToken)
-            //{
-            //    var validationSets = _dataProvider.GetAllValidationSets();
-            //    var result = new List<ExtensionValidationSetNode>();
-            //    var alternateValues = _alternateValueProvider.GetAlternateValuesForExtensionAsync(RequestContext.Internal, source.Id, cancellationToken).Result;
-
-            //    var extensionValidationSetNode = new ExtensionValidationSetNode();
-                
-            //    foreach (var valset in validationSets)
-            //    {
-            //        var usedRecordCount = 0;
-            //        var valSetEntries = await GetEntriesForValidationSetAsync(valset.Id, cancellationToken).ConfigureAwait(false);
-            //        var numberOfValsetEntries = valSetEntries.Count;
-            //        foreach (var entry in valSetEntries)
-            //        {
-            //            foreach (var altValue in alternateValues)
-            //            {
-            //                if (altValue.UsageTypeId == UsageType.ValidationSetEntry && altValue.RecordId == entry.Id)
-            //                {
-            //                    usedRecordCount++;
-            //                }
-            //            }
-            //        }
-            //        result.Add(new ExtensionValidationSetNode
-            //        {
-            //            ValidationSetName = valset.Name,
-            //            ValidationSetId = valset.Id,
-            //            TotalRecordCount = numberOfValsetEntries, // the number of Validation Set Entries that belong to that validation set
-            //            UsedRecordCount = usedRecordCount  // the number of ValSetEntries that are tied to an alterate value
-            //        });
-            //    }
-            //    return result;
-            //}
 
