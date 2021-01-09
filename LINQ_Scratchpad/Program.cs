5bd6c9624da7f7c7f88664c6a7188fd77ea5a76d
Blair Brown <blair.j.brown@gmail.com>
2021-01-09 00:09:07 -0500

    added .Distinct to the .SelectMany method

--- LINQ_Scratchpad/Program.cs
+++ LINQ_Scratchpad/Program.cs
@@ -11,7 +11,7 @@ namespace LINQ_Scratchpad
         {
             List<Dog> listOfDogs = new List<Dog>()
             {
-                new Dog("Rex", new List<string> {"ball", "sniffing"}, 2) ,
+                new Dog("Rex", new List<string> {"ball", "sniffing", "Frisbee"}, 2),
                 new Dog("Peanut", new List<string> { "Frisbee", "fetch" }, 7)
             };
             // calling without the optional parameter will default to filtering on the name Peanut
@@ -22,7 +22,7 @@ namespace LINQ_Scratchpad
 
             FormatDogInfoUsingSELECT(listOfDogs);
 
-            PrintDogsMadSkillz(listOfDogs);
+            PrintDogsAllDogsSkills(listOfDogs);
         }
 
         // .Where
@@ -46,10 +46,26 @@ namespace LINQ_Scratchpad
             }
         }
         // .SelectMany
+        //The SelectMany in LINQ is used to project each element of a sequence to an IEnumerable<T> and then flatten the resulting sequences into 
+        //one sequence.That means the SelectMany operator combines the records from a sequence of results and then converts it into one result.
 
-        public static void PrintDogsMadSkillz(List<Dog> listOfDogs) {
+        //Distinct will remove any duplicates from the IEnumerable that is returned. So in the second example, it doesn't print Frisbee twice, just once.
+        public static void PrintDogsAllDogsSkills(List<Dog> listOfDogs) {
             var listOfAllDogsSkills = listOfDogs.SelectMany(d => d.Skills);
-            
+            var listOfAllDistinctDogsSkills = listOfDogs.SelectMany(d => d.Skills).Distinct();
+
+            Console.WriteLine("All dog skills: ");
+            foreach (var skill in listOfAllDogsSkills)
+            {
+                Console.WriteLine(skill);
+            }
+
+            Console.WriteLine("All distinct dog skills: ");
+            foreach (var skill in listOfAllDistinctDogsSkills)
+            {
+                Console.WriteLine(skill);
+            }
+
         }
 
         // .GroupBy
